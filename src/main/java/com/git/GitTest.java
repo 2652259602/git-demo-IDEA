f05069d7fbc6e1218d0dee52ef746ab85a9ec52a
hyx <2652259602@qq.com>
2022-07-20 11:43:51 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author Yuxiong Huang
+ * @create 2022-07-20 11:41
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+
+}
