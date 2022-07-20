5466b14c52c468190534b1dc8f10d2a59ba9583b
hyx <2652259602@qq.com>
2022-07-20 12:13:30 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -2,12 +2,13 @@ package com.git;
 
 /**
  * @author Yuxiong Huang
- * @create 2022-07-20 11:41
+ * @create 2022-07-20 11:44
  */
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello, git!");
+        System.out.println("hello, git2!");
     }
 
 }
